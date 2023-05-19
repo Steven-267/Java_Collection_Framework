a726a62ebae2a1f3031dfdeee9cdab46155e55c7
Steven-267 <aa030726@126.com>
2023-05-20 00:32:22 +0800

    ggs

---
+++ JCol01-20230514/src/ggs.java
@@ -0,0 +1,11 @@
+/**
+ * @author Steven
+ * @ClassName ggs.java
+ * @Description
+ * @createTime 2023-05-20 00:22
+ **/
+public class ggs {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
